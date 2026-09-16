f8f426e200e5e826c880356e148da2cc1745faaf
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-09-16 17:34:28 +0300

    Fix alphabet string formatting

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-print("".join(letter for letter in alphabet if letter not in "qe"))
+print("{}".format("".join(letter for letter in alphabet if letter not in "qe")))
