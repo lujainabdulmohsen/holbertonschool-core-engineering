58c3c57a6f00b532cde19376f081c051a1ed6900
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-09-16 17:35:14 +0300

    Fix alphabet output newline

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-print("{}".format("".join(letter for letter in alphabet if letter not in "qe")))
+print("{}".format("".join(letter for letter in alphabet if letter not in "qe")),
+      end="")
