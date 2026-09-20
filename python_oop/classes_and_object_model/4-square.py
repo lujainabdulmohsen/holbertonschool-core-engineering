63a12924acd059e2cd474fe4b9bcc1b61e65a8f8
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-09-20 17:58:58 +0300

    Add Square size property

---
+++ python_oop/classes_and_object_model/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize a square."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Return the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set and validate the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
