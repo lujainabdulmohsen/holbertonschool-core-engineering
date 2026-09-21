e60c34458bc02b843447b87159e5839213e09227
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-09-21 18:45:58 +0300

    Fix Rectangle inheritance

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Defines a Rectangle class."""
 
-from base_geometry import BaseGeometry
+BaseGeometry = __import__('base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
